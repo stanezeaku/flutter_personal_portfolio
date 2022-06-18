444445aa75ae26dcfdcbfba564680cd86f2c46b4
Ezeaku Ifechukwu Stanley <56836874+stanezeaku@users.noreply.github.com>
2022-06-18 12:11:06 +0400

    CV update

--- lib/screens/main/components/side_menu.dart
+++ lib/screens/main/components/side_menu.dart
@@ -51,7 +51,7 @@ class SideMenu extends StatelessWidget {
                       TextButton(
                         onPressed: () {
                           method.launchURL(
-                              "https://drive.google.com/file/d/1oFEABXgZjoIZacENK5B4z1pbjofKKQ_7/view?usp=sharing");
+                              "https://drive.google.com/file/d/16BzEEmY8uao2AX33gWhEa2YSL7p_iHMP/view?usp=sharing");
                         },
                         child: FittedBox(
                           child: Row(
